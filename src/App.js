23daa6c8ad5715ab86ead73d66330fcd62479c2c
Jeet Vora <jeetvora331@gmail.com>
2022-09-21 04:53:45 +0530

    Add all my files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import Scroll from "./Scroll";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			state_robots: robots,
+			state_robots: [],
 			searchField: "",
 		};
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => this.setState({ state_robots: users }));
+		// this.setState({ state_robots: robots });
 	}
 
 	onSearch = (event) => {
@@ -24,8 +32,13 @@ class App extends Component {
 		console.log(newRobots);
 		return (
 			<div className="tc">
+				<h1 className="f1" style={{ fontFamily: "consolas" }}>
+					RoboFriends
+				</h1>
 				<SearchBox onSearch={this.onSearch} />
-				<CardList var_robots={newRobots} />
+				<Scroll>
+					<CardList var_robots={newRobots} />
+				</Scroll>
 			</div>
 		);
 	}
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,12 @@ import React from "react";
 const SearchBox = ({ onSearch }) => {
 	return (
 		<div className="pa2">
-			<input type="search" placeholder="Enter Name" onChange={onSearch} />
+			<input
+				className=" bg-washed-yellow  pa2 br shadow-5"
+				type="search"
+				placeholder="Enter Name"
+				onChange={onSearch}
+			/>
 		</div>
 	);
 };
